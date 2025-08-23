2508d5af2892685f5a9bf2373f62a58487bb5f2c
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-23 22:47:50 +0300

    update

--- modules/postgresql/main.tf
+++ modules/postgresql/main.tf
@@ -3,6 +3,7 @@ resource "helm_release" "postgresql" {
   name             = "postgresql-ha"
   repository       = "https://charts.bitnami.com/bitnami"
   chart            = "postgresql-ha"
+  version          = "16.2.2"
   namespace        = "default"
   create_namespace = true
   timeout          = 600
