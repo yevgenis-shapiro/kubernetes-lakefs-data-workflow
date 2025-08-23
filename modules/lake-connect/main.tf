c2404c4f93c95fa946a9525a7ec7626b5c4035b6
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-24 02:35:56 +0300

    update

--- modules/lake-connect/main.tf
+++ modules/lake-connect/main.tf
@@ -1,13 +1,6 @@
 
 provider "lakefs" {
-  endpoint   = "http://lakefs.lakefs.svc.cluster.local:80"
-  access_key = "AKIAJ25JLGWO747UT5FQ"
-  secret_key = "xtjzyzES3sEzwhyaZHEY4vzEvNhtTfb6Fty4kAye"
-}
-
-resource "lakefs_repository" "example" {
-  repository_id    = "my-repo"
-  storage_namespace = "s3://lakefs/"
-  description      = "Example lakeFS repo on MinIO"
-  default_branch   = "main"
+  endpoint   = var.lakefs_endpoint
+  access_key = var.lakefs_access_key
+  secret_key = var.lakefs_secret_key
 }
