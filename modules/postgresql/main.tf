b38dfde2a923b266105cda906bc8f737a84ca9d2
Yevgeni Shapiro <23049337+yevgenis-shapiro@users.noreply.github.com>
2025-08-23 21:47:11 +0300

    update

--- modules/postgresql/main.tf
+++ modules/postgresql/main.tf
@@ -8,45 +8,7 @@ resource "helm_release" "postgresql" {
   timeout          = 600
   wait             = true
 
-  set {
-    name  = "postgresql.username"
-    value = "postgres"
-  }
-
-  set {
-    name  = "postgresql.password"
-    value = "postgres"
-  }
-}
-
-resource "kubernetes_job" "create_lakefs_db" {
-  metadata {
-    name      = "create-lakefs-db"
-    namespace = helm_release.postgresql.namespace
-  }
-
-  spec {
-    template {
-      spec {
-        restart_policy = "OnFailure"
-
-        container {
-          name  = "create-db"
-          image = "bitnami/postgresql:15"
-          command = ["psql"]
-          args = [
-            "-h", "postgresql-ha-postgresql-ha-pgpool",
-            "-U", "postgres",
-            "-c", "CREATE DATABASE lakefs;"
-          ]
-          env {
-            name  = "PGPASSWORD"
-            value = "postgres"
-          }
-        }
-      }
-    }
-  }
-
-  depends_on = [helm_release.postgresql]
+  values = [
+    file("${path.module}/values-postgresql.yaml")
+  ]
 }
